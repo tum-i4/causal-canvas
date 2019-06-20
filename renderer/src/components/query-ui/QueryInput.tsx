053d3486fd48edfc4ad869f38288ec7581182c98
unknown <t.klesel@gmx.de>
2019-06-20 11:06:45 +0200

    fix renamed node as phi

--- renderer/src/components/query-ui/QueryInput.tsx
+++ renderer/src/components/query-ui/QueryInput.tsx
@@ -62,195 +62,198 @@ const FirstLabel = styled.div`
 `
 
 interface IQueryInputState {
-    solvingStrategy: string;
-    phi: string;
-    causeSearch: string;
-    cause: {
-        name: string;
-        value: boolean;
-    }[];
-    context: {
-        name: string;
-        value: boolean;
-    }[];
+	solvingStrategy: string;
+	phi: string;
+	causeSearch: string;
+	cause: {
+		name: string;
+		value: boolean;
+	}[];
+	context: {
+		name: string;
+		value: boolean;
+	}[];
 }
 
 interface IQueryInputProps {
-    width: number;
-    graph: Graph;
-    query: (query: IQueryData) => void;
+	width: number;
+	graph: Graph;
+	query: (query: IQueryData) => void;
 }
 
 const SolvingStrategys = [
-    'BRUTE_FORCE',
-    'BRUTE_FORCE_OPTIMIZED_W',
-    'SAT',
-    'SAT_MINIMAL',
-    'SAT_COMBINED',
-    'SAT_COMBINED_MINIMAL',
-    'SAT_OPTIMIZED_W',
-    'SAT_OPTIMIZED_W_MINIMAL',
-    'SAT_OPTIMIZED_FORMULAS',
-    'SAT_OPTIMIZED_FORMULAS_MINIMAL',
-    'SAT_OPTIMIZED_AC3',
-    'SAT_OPTIMIZED_AC3_MINIMAL'
+	'BRUTE_FORCE',
+	'BRUTE_FORCE_OPTIMIZED_W',
+	'SAT',
+	'SAT_MINIMAL',
+	'SAT_COMBINED',
+	'SAT_COMBINED_MINIMAL',
+	'SAT_OPTIMIZED_W',
+	'SAT_OPTIMIZED_W_MINIMAL',
+	'SAT_OPTIMIZED_FORMULAS',
+	'SAT_OPTIMIZED_FORMULAS_MINIMAL',
+	'SAT_OPTIMIZED_AC3',
+	'SAT_OPTIMIZED_AC3_MINIMAL'
 ]
 
 export class QueryInput extends Component<IQueryInputProps, IQueryInputState> {
 
-    constructor(props: IQueryInputProps) {
-        super(props);
+	constructor(props: IQueryInputProps) {
+		super(props);
 
-        const context = props.graph.getCurrentGraph().nodes.filter(n => n.isExogenousVariable).map(n => ({ name: n.title, value: n.value }))
+		const context = props.graph.getCurrentGraph().nodes.filter(n => n.isExogenousVariable).map(n => ({ name: n.title, value: n.value }))
 
-        this.state = {
-            solvingStrategy: SolvingStrategys[0],
-            phi: '',
-            cause: [],
-            causeSearch: '',
-            context
-        }
-    }
+		this.state = {
+			solvingStrategy: SolvingStrategys[0],
+			phi: '',
+			cause: [],
+			causeSearch: '',
+			context
+		}
+	}
 
-    public setQuery = (data: IQueryData) => {
-        this.setState({
-            ...data
-        })
-    }
+	public setQuery = (data: IQueryData) => {
+		this.setState({
+			...data
+		})
+	}
 
 
-    onSolvingStrategyChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        this.setState({
-            ...this.state,
-            solvingStrategy: event.target.value
-        })
-    }
+	onSolvingStrategyChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+		this.setState({
+			...this.state,
+			solvingStrategy: event.target.value
+		})
+	}
 
-    onPhiUpdate = (phi: string) => {
-        this.setState({
-            ...this.state,
-            phi
-        })
-    }
+	onPhiUpdate = (phi: string) => {
+		const selectedNode = this.props.graph.getCurrentGraph().nodes.find(n => n.id === phi)!
+		if (selectedNode !== undefined) {
+			this.setState({
+				...this.state,
+				phi: selectedNode.title
+			})
+		}
+	}
 
-    onCauseSearchChange = (value: string) => {
-        this.setState({
-            ...this.state,
-            causeSearch: value
-        })
-    }
+	onCauseSearchChange = (value: string) => {
+		this.setState({
+			...this.state,
+			causeSearch: value
+		})
+	}
 
-    onCauseSearchSubmit = () => {
-        this.setState({
-            ...this.state,
-            causeSearch: '',
-            cause: [{
-                name: this.state.causeSearch,
-                value: true
-            }, ...this.state.cause],
-        })
-    }
+	onCauseSearchSubmit = () => {
+		this.setState({
+			...this.state,
+			causeSearch: '',
+			cause: [{
+				name: this.state.causeSearch,
+				value: true
+			}, ...this.state.cause],
+		})
+	}
 
-    removeCauseItem = (idx: number) => {
-        this.setState({
-            ...this.state,
-            cause: this.state.cause.filter((v, i) => i !== idx)
-        })
-    }
+	removeCauseItem = (idx: number) => {
+		this.setState({
+			...this.state,
+			cause: this.state.cause.filter((v, i) => i !== idx)
+		})
+	}
 
-    toggelCauseItem = (idx: number) => {
-        this.setState({
-            ...this.state,
-            cause: this.state.cause.map((v, i) => i === idx ? { ...v, value: !v.value } : v)
-        })
-    }
+	toggelCauseItem = (idx: number) => {
+		this.setState({
+			...this.state,
+			cause: this.state.cause.map((v, i) => i === idx ? { ...v, value: !v.value } : v)
+		})
+	}
 
-    toggelContextItem = (idx: number) => {
-        this.setState({
-            ...this.state,
-            context: this.state.context.map((v, i) => i === idx ? { ...v, value: !v.value } : v)
-        })
-    }
+	toggelContextItem = (idx: number) => {
+		this.setState({
+			...this.state,
+			context: this.state.context.map((v, i) => i === idx ? { ...v, value: !v.value } : v)
+		})
+	}
 
-    onQueryClick = () => {
+	onQueryClick = () => {
 
-        const { phi, solvingStrategy, cause, context } = this.state;
+		const { phi, solvingStrategy, cause, context } = this.state;
 
-        this.props.query({
-            phi: forumlaToJavaFormula(phi),
-            solvingStrategy,
-            cause: cause,
-            context
-        });
-    }
+		this.props.query({
+			phi: forumlaToJavaFormula(phi),
+			solvingStrategy,
+			cause: cause,
+			context
+		});
+	}
 
-    public render() {
+	public render() {
 
-        const { solvingStrategy, phi, causeSearch, cause, context } = this.state;
-        const { width } = this.props;
+		const { solvingStrategy, phi, causeSearch, cause, context } = this.state;
+		const { width } = this.props;
 
-        return <QueryInputContainer
-            width={width}
-        >
-            <Scrollbars style={{ width: '100%', height: '100%' }}>
+		return <QueryInputContainer
+			width={width}
+		>
+			<Scrollbars style={{ width: '100%', height: '100%' }}>
 
-                <FirstLabel>
-                    Context
+				<FirstLabel>
+					Context
             </FirstLabel>
-                <CauseContainer>
-                    <Scrollbars style={{ width: '100%', height: '100%' }}>
-                        {
-                            context.map((c, idx) => <CauseListItem key={`context-${name}${idx}`} {...c} idx={idx} toggel={this.toggelContextItem} />)
-                        }
-                    </Scrollbars>
-                </CauseContainer>
-                <Label>
-                    Cause
+				<CauseContainer>
+					<Scrollbars style={{ width: '100%', height: '100%' }}>
+						{
+							context.map((c, idx) => <CauseListItem key={`context-${name}${idx}`} {...c} idx={idx} toggel={this.toggelContextItem} />)
+						}
+					</Scrollbars>
+				</CauseContainer>
+				<Label>
+					Cause
             </Label>
-                <QueryNodeInput
-                    value={causeSearch}
-                    onChange={this.onCauseSearchChange}
-                    onSubmit={this.onCauseSearchSubmit}
-                    suggsestions={this.props.graph.getCurrentGraph().nodes.filter(n => !n.isExogenousVariable).map(n => n.title)}
-                />
-                <CauseContainer>
-                    <Scrollbars style={{ width: '100%', height: '100%' }}>
-                        {
-                            cause.map((c, idx) => <CauseListItem key={`cause-${name}${idx}`} {...c} idx={idx} remove={this.removeCauseItem} toggel={this.toggelCauseItem} />)
-                        }
-                    </Scrollbars>
-                </CauseContainer>
-                <Label>
-                    Phi
+				<QueryNodeInput
+					value={causeSearch}
+					onChange={this.onCauseSearchChange}
+					onSubmit={this.onCauseSearchSubmit}
+					suggsestions={this.props.graph.getCurrentGraph().nodes.filter(n => !n.isExogenousVariable).map(n => n.title)}
+				/>
+				<CauseContainer>
+					<Scrollbars style={{ width: '100%', height: '100%' }}>
+						{
+							cause.map((c, idx) => <CauseListItem key={`cause-${name}${idx}`} {...c} idx={idx} remove={this.removeCauseItem} toggel={this.toggelCauseItem} />)
+						}
+					</Scrollbars>
+				</CauseContainer>
+				<Label>
+					Phi
             </Label>
-                <PhiContainer>
-                    <NewFormulaInput
-                        formula={phi}
-                        onChange={this.onPhiUpdate}
-                        nodes={this.props.graph.getCurrentGraph().nodes}
-                    />
-                </PhiContainer>
-                <Label>
-                    SolvingStrategy
+				<PhiContainer>
+					<NewFormulaInput
+						formula={phi}
+						onChange={this.onPhiUpdate}
+						nodes={this.props.graph.getCurrentGraph().nodes}
+					/>
+				</PhiContainer>
+				<Label>
+					SolvingStrategy
             </Label>
-                <Select
-                    style={{
-                        width: '100%'
-                    }}
-                    value={solvingStrategy}
-                    onChange={this.onSolvingStrategyChange}
-                    input={<Input name="age" id="age-helper" />}
-                >
-                    {SolvingStrategys.map(s => <MenuItem key={s} value={s}>{s}</MenuItem>)}
-                </Select>
-                <ButtonContainer>
-                    <QueryButton
-                        onClick={this.onQueryClick}
-                    >
-                        Query
+				<Select
+					style={{
+						width: '100%'
+					}}
+					value={solvingStrategy}
+					onChange={this.onSolvingStrategyChange}
+					input={<Input name="age" id="age-helper" />}
+				>
+					{SolvingStrategys.map(s => <MenuItem key={s} value={s}>{s}</MenuItem>)}
+				</Select>
+				<ButtonContainer>
+					<QueryButton
+						onClick={this.onQueryClick}
+					>
+						Query
                 </QueryButton>
-                </ButtonContainer>
-            </Scrollbars>
-        </QueryInputContainer>
-    }
+				</ButtonContainer>
+			</Scrollbars>
+		</QueryInputContainer>
+	}
 }
